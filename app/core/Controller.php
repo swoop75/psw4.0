a13a32248b1c4a6705ac10f62072b745110fb579
swoop75 <laoan.ada@outlook.com>
2025-07-20 08:24:45 +0200

    2025-07-15 13:00: Resolved fatal error in Router by correcting relative include paths. Adjusted Controller's view-loading method and view templates to correctly locate the `templates` directory inside `/public`.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,22 +1,55 @@
 <?php
-// c:/Users/laoan/Documents/GitHub/psw/psw4.0/app/core/Controller.php
+// c:/Users/laoan/Documents/GitHub/psw/psw4.0/app/core/Router.php
 
-/**
- * Base Controller
- * Loads the models and views
- */
-class Controller {
-    // Load view and pass data
-    public function view($view, $data = []) {
-        // Check for the view file, noting that templates are inside the public folder
-        $viewPath = APPROOT . '/public/templates/' . $view . '.php';
-        if (file_exists($viewPath)) {
-            // The 'extract' function turns array keys into variables (e.g., $data['title'] becomes $title)
-            extract($data);
-            require_once $viewPath;
-        } else {
-            // View does not exist - die with a helpful error message
-            die('Error: View does not exist at path: ' . $viewPath);
+class Router {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct(){
+        $url = $this->getUrl();
+
+        // Look for controller in controllers folder
+        if(isset($url[0]) && file_exists('../controllers/' . ucwords($url[0]) . '.php')){
+            // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 Index
+            unset($url[0]);
         }
+
+        // Require the controller
+        // First, load the base controller which all other controllers will extend
+        require_once 'Controller.php';
+        // Then, load the specific controller for the page
+        require_once '../controllers/'. $this->currentController . '.php';
+
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
+
+        // Check for second part of url (the method)
+        if(isset($url[1])){
+            // Check to see if method exists in controller
+            if(method_exists($this->currentController, $url[1])){
+                $this->currentMethod = $url[1];
+                // Unset 1 index
+                unset($url[1]);
+            }
+        }
+
+        // Get params - The rest of the URL parts
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+    }
+
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+        return [];
     }
 }
