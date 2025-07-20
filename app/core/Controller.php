2d81b3203cda3a07efc60f8f92a9492aea34ccf4
swoop75 <laoan.ada@outlook.com>
2025-07-20 08:28:11 +0200

    2025-07-15 14:00: Fixed "Cannot declare class" fatal error by correcting the content of `app/core/Controller.php`. Cleaned up multiple other files that had incorrect, copy-pasted content to prevent subsequent errors.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,55 +1,8 @@
-<?php
-// c:/Users/laoan/Documents/GitHub/psw/psw4.0/app/core/Router.php
+# c:/Users/laoan/Documents/GitHub/psw/psw4.0/public/.htaccess
 
-class Router {
-    protected $currentController = 'Pages';
-    protected $currentMethod = 'index';
-    protected $params = [];
-
-    public function __construct(){
-        $url = $this->getUrl();
-
-        // Look for controller in controllers folder
-        if(isset($url[0]) && file_exists('../controllers/' . ucwords($url[0]) . '.php')){
-            // If exists, set as controller
-            $this->currentController = ucwords($url[0]);
-            // Unset 0 Index
-            unset($url[0]);
-        }
-
-        // Require the controller
-        // First, load the base controller which all other controllers will extend
-        require_once 'Controller.php';
-        // Then, load the specific controller for the page
-        require_once '../controllers/'. $this->currentController . '.php';
-
-        // Instantiate controller class
-        $this->currentController = new $this->currentController;
-
-        // Check for second part of url (the method)
-        if(isset($url[1])){
-            // Check to see if method exists in controller
-            if(method_exists($this->currentController, $url[1])){
-                $this->currentMethod = $url[1];
-                // Unset 1 index
-                unset($url[1]);
-            }
-        }
-
-        // Get params - The rest of the URL parts
-        $this->params = $url ? array_values($url) : [];
-
-        // Call a callback with array of params
-        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
-    }
-
-    public function getUrl(){
-        if(isset($_GET['url'])){
-            $url = rtrim($_GET['url'], '/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
-            return $url;
-        }
-        return [];
-    }
-}
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.*)$ index.php?url=$1 [L,QSA]
+</IfModule>
--- public/templates/includes/header.php
+++ public/templates/includes/header.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo SITENAME; ?></title>
+    <title><?php echo $title ?? SITENAME; ?></title>
     <!-- Font Awesome for Icons (as per design_document.txt) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <!-- Google Fonts (as per design_document.txt) -->
